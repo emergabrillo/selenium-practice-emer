1705fc2e0b2fa7ddf541a998623e36cf263ba5f8
Emer Gabrillo <emcleegab@gmail.com>
2026-06-29 05:36:49 +0800

    Added negative tests for incorrect username & password

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.common.by import By
 from pages.login_page import LoginPage
 
 def test_successful_login(driver):
@@ -10,3 +11,28 @@ def test_successful_login(driver):
     
     # Verify successful login by checking for a specific element or URL change
     assert "Logged In Successfully" in driver.page_source  # Checks if the success message is present in the page source after login
+
+def test_incorrect_username(driver):
+    driver.get("https://practicetestautomation.com/practice-test-login/")
+    
+    login_page = LoginPage(driver)
+    login_page.enter_username("wrong_user")
+    login_page.enter_password("Password123")
+    login_page.click_login()
+    
+    # Verify incorrect username by checking for an error message
+    error_warning = driver.find_element(By.ID, "error")
+    assert error_warning.is_displayed()  # Checks if the error message is displayed
+    assert error_warning.text == "Your username is invalid!"  # Validates the specific error message for incorrect username
+
+def test_incorrect_password(driver):
+    driver.get("https://practicetestautomation.com/practice-test-login/")
+    login_page = LoginPage(driver)
+    login_page.enter_username("student")
+    login_page.enter_password("password321")
+    login_page.click_login()
+    
+    # Verify incorrect login by checking for an error message
+    error_warning = driver.find_element(By.ID, "error")
+    assert error_warning.is_displayed()  # Checks if the error message is displayed
+    assert error_warning.text == "Your password is invalid!"  # Validates the specific error message for incorrect password
